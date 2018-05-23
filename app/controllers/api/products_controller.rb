a954a9a88a692112c62e23db3b6f13343d18a5dd
saintlove <saintlovett@gmail.com>
2018-05-22 21:33:55 -0500

    created Suppliers model, added sort by info

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,30 @@
 class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    search_term = params[:search]
+    
+    if search_term
+      @products = @products.where("name iLIKE ? OR description iLIKE ?", "%#{search_term}%", "%#{search_term}%")
+    end
+
+
+
+    sort_attribute = params[:sort_by]
+    sort_order = params[:sort_order]
+
+
+    
+    if sort_attribute && sort_order
+      @products = @products.order(sort_attribute => sort_order)
+    elsif sort_attribute
+       @products = @products.order(sort_attribute => :asc)
+    else
+      @products = Product.order(:id => :asc)
+    end
+
+    
+
     render 'index.json.jbuilder'
   end
 
